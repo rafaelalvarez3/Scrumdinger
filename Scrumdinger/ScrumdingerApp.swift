4fd19fd7b06d2a486d2f913f174be1295f51fcb2
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-08 15:42:54 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
