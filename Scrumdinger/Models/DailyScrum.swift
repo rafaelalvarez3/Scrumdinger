13a585bfcee643a6a0b6944bd3f2ac6fb2fb5051
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-08 17:23:52 -0500

    Created the color theme and started the daily scrum model.

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,15 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/8/24.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
