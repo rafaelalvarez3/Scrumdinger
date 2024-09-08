31f1a05ec46574bff391ba3d4ff254a5563a78bc
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-08 17:47:13 -0500

    Refactored the preview for MeetingView() and created the card view.

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,24 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/8/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
