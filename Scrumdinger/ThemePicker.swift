bda8c9474cfff0467765940257396ab55705644e
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-14 12:17:57 -0500

    Added theme picker view.

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/14/24.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker()
+    }
+}
