4622c3c0a2ab8d77af87f9023527284fde5387de
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-14 10:29:41 -0500

    Created a new SwiftUI view DetailEditView()

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/14/24.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
