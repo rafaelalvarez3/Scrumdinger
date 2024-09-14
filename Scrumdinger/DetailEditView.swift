b12b6c5d3be6e275069a9ff070be8e28774a6d42
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-14 10:39:17 -0500

    Inside Section, add a TextField for the meeting title. TextField takes a binding to a String. You can use the $ syntax to create a binding to scrum.title. The current view manages the state of the data property.

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+            }
+        }
     }
 }
 
