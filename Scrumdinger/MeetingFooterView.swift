8c6f7fd1b5331790c379a5b35c3f8acf5466e123
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-15 11:30:46 -0500

    Created MeetingFooterView.

---
+++ Scrumdinger/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/15/24.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
