f8485c6d41cd76abee42442c65e1fea52911319e
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-15 10:46:07 -0500

    Added design elements and created custom style. Added new view style ScumProgressView. New style not yet finished.

---
+++ Scrumdinger/ScrumProgressViewStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by Rafael Alvarez on 9/15/24.
+//
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: View {
+    var theme: Theme
+}
+
+struct ScrumProgressViewStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        
+    }
+}
