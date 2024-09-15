502253af1ba62659c98232cacc914b864474a5f3
Rafael Alvarez <rafaelalvarez3@proton.me>
2024-09-15 10:03:37 -0500

    Finished isolating the meeting header. Added preview layout modifier to have an accurate representation of the view.

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct MeetingHeaderView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
-        HStack {
-            VStack(alignment: .leading) {
-                Text("Seconds Elapsed")
-                    .font(.caption)
-                Label("300", systemImage: "hourglass.tophalf.fill")
-            }
-            Spacer()
-            VStack(alignment: .trailing) {
-                Text("Seconds Remaining")
-                    .font(.caption)
-                Label("600", systemImage: "hourglass.bottomhalf.fill")
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
             }
         }
         .accessibilityElement(children: .ignore)
@@ -32,5 +34,6 @@ struct MeetingHeaderView: View {
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
         MeetingHeaderView()
+            .previewLayout(.sizeThatFits)
     }
 }
